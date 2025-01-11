9c81bd523041318d4f384bec64484ece3ba32b66
Charles Doucet <charles.douc@gmail.com>
2025-01-11 18:51:26 -0500

    feat: basic vertical navigation on left side

--- src/views/vertical-nav.ts
+++ src/views/vertical-nav.ts
@@ -1,14 +1,57 @@
-import '../../public/styles/vertical-nav.css'
+import '../../public/styles/vertical-nav.css';
+import cdIcon from "../../public/img/common/cd_icon_green.png";
+
+let menuTitle: string = "CD-BASH";
+let footerCopyrights: string = "© 2025 Charles Doucet - All Rights Reserved";
+
 
 export function VerticalNav() {
     const menuBox = document.createElement("div");
     menuBox.id = "vertical-nav";
 
+    menuBox.appendChild(NavHeader());
+    menuBox.appendChild(NavFooter());
+
     return menuBox;
-    
+}
+
+// -------------------------------------------------------------------
+
+function NavHeader() {
+    const header = document.createElement("div");
+    const topTriangle = document.createElement("div");
+    const info = document.createElement("div");
+    const icon = document.createElement("img");
+    const title = document.createElement("h5");
+
+    header.className = "header";
+    topTriangle.className = "top-triangle";
+    info.className = "info";
+    icon.className = "icon";
+    title.className = "title";
+
+    icon.src = cdIcon;
+    title.textContent = menuTitle;
+
+    info.appendChild(icon);
+    info.appendChild(title);
+    header.appendChild(topTriangle);
+    header.appendChild(info);
+
+    return header;
 }
 
 
-function NavHeader() {
-    const 
+function NavFooter() {
+    const footer = document.createElement("div");
+    const copyrights = document.createElement("p");
+
+    footer.className = "nav-footer";
+    copyrights.className = "copyrights";
+
+    copyrights.textContent = footerCopyrights
+
+    footer.appendChild(copyrights);
+
+    return footer;
 }
