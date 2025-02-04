d6d6d524fce7a098adb4632dca85eee74c45dba8
Charles Doucet <charles.douc@gmail.com>
2025-02-04 11:02:03 -0500

    feat: 3d canvas in the vertical nav

--- src/components/vertical-nav/project-data-cube.ts
+++ src/components/vertical-nav/project-data-cube.ts
@@ -1,25 +1,30 @@
 ﻿import * as THREE from 'three';
 
+// TO REVIEW
+// @ts-ignore
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
 
 export const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( 230, 230 );
 renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+
+const controls = new OrbitControls( camera, renderer.domElement );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 2;
+controls.panSpeed = 0;
+controls.rotateSpeed = 0.5;
+controls.update();
+
+//-----------------------------------------------------------------------
 
 function animate() {
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
     renderer.render( scene, camera );
-
 }
