2c4f4f1ad1ac93c9e870477d63fc7d641204fd71
Charles Doucet <charles.douc@gmail.com>
2025-02-05 17:07:28 -0500

    wip: list item list for interactive page

---
+++ src/components/topic-list-item/index.ts
@@ -0,0 +1,39 @@
+﻿
+export type ItemContent = {
+    readonly showcase: boolean;
+    readonly thumbnail: string;
+    readonly title: string;
+    readonly description: string;
+    readonly category: string;
+}
+
+//-----------------------------------------------------------------------
+
+export function createTopicListItem(content: ItemContent) {
+    const itemBox = document.createElement('div');
+    const itemThumbnail = document.createElement('img');
+    const itemTitle = document.createElement('h5');
+    const itemDescription = document.createElement('p');
+    const itemCategory = document.createElement('p');
+
+    itemBox.className = 'item-box';
+
+    if (content.showcase) {
+        itemBox.className += 'item-showcase';
+    }
+    else {
+        itemBox.className += 'item-list';
+    }
+
+    itemThumbnail.src = content.thumbnail;
+    itemTitle.textContent = content.title;
+    itemDescription.textContent = content.description;
+    itemCategory.textContent = content.category;
+
+    itemBox.appendChild(itemThumbnail);
+    itemBox.appendChild(itemTitle);
+    itemBox.appendChild(itemDescription);
+    itemBox.appendChild(itemCategory);
+
+    return itemBox;
+}
