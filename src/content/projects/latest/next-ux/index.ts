f2a99a2b7707c4a172caa5502fad01b7492f94d1
Charles Doucet <charles.douc@gmail.com>
2025-02-27 22:57:09 -0500

    feat: [#32] separate categories for projects

---
+++ src/content/projects/latest/next-ux/index.ts
@@ -0,0 +1,48 @@
+﻿import {createProjectContent} from "../../template.ts";
+
+import SHOWCASE_WEBM from "./assets/next-ux-showcase.webm";
+import SHOWCASE_MP4 from "./assets/next-ux-showcase.mp4";
+
+import SCREENSHOT_1 from "./assets/next-ux-screenshot-1.jpg"
+import SCREENSHOT_2 from "./assets/next-ux-screenshot-2.jpg"
+import SCREENSHOT_3 from "./assets/next-ux-screenshot-3.jpg"
+import SCREENSHOT_4 from "./assets/next-ux-screenshot-4.jpg"
+
+import THUMBNAIL from "./assets/next-ux-thumbnail.png";
+
+//-----------------------------------------------------------------------
+
+export const {content, techs, buttons, thumbnail} = createProjectContent(
+    "Next Ux", // Title
+    "web app ux & ui - 2021", // Subtitle
+    "Clear Workflows", // Tagline
+
+    // Paragraphs
+    [
+        "Complete overhaul of Voxco's product line, including a redesigned user flow and interface."
+    ],
+
+    // Medias
+    [SHOWCASE_WEBM, SHOWCASE_MP4],
+    [SCREENSHOT_1, SCREENSHOT_2, SCREENSHOT_3, SCREENSHOT_4],
+
+    // Techs Used
+    [
+        {technology: "React", percentage: 30},
+        {technology: "Blazor", percentage: 40},
+        {technology: "Adobe XD", percentage: 80}
+    ],
+
+    //Buttons
+    [
+        ["LABEL", "LINK", true],
+        ["LABEL", "LINK", false]
+    ],
+
+    // Thumbnail
+    THUMBNAIL,
+    "UX initiative taken over Voxco’s software.",
+    "Ux / Ui Design",
+    "#0C0A1F",
+    "next-ux"
+);
--- src/views/interactive.ts
+++ src/views/interactive.ts
@@ -1,27 +1,29 @@
 ﻿import {writeTitle} from "./utils";
-import {buildThumbnailList} from "../content/projects";
+import {buildThumbnailList, ProjectCategory} from "../content/projects";
 
 
 //-----------------------------------------------------------------------
 
 export function interactiveView() {
     const article = document.createElement('article');
-    const latestProject = document.createElement('section');
-    const listProject = document.createElement('div');
+    const pageTitle = writeTitle("h1", "project collection");
+    const latestEntries = projectList("Latest Entries", "latest");
+    const archives = projectList("Archives", "archive");
 
-    const viewTitle = writeTitle("h1", "project collection");
-    const allProjectTitle = writeTitle("h2", "latest entries");
-
-    latestProject.className = 'latest-project';
-    listProject.className = 'list-project';
-
-    article.appendChild(viewTitle);
-    article.appendChild(latestProject);
-    article.appendChild(listProject);
-    listProject.appendChild(allProjectTitle);
-    listProject.appendChild(buildThumbnailList());
+    article.appendChild(pageTitle);
+    article.appendChild(latestEntries);
+    article.appendChild(archives);
     
     return article;
 }
 
 
+function projectList(title: string, reference: ProjectCategory = 'latest') {
+    const projectSection = document.createElement('section');
+    const sectionTitle = writeTitle("h2", title);
+
+    projectSection.appendChild(sectionTitle);
+    projectSection.appendChild(buildThumbnailList(reference));
+
+    return projectSection;
+}
