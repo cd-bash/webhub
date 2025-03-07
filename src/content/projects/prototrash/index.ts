e1d20692b467aba63c6ccc15bc475438d584e358
Charles Doucet <charles.douc@gmail.com>
2025-03-07 18:39:07 -0500

    feat: fit in prototrash content

--- src/content/projects/prototrash/index.ts
+++ src/content/projects/prototrash/index.ts
@@ -1,5 +1,7 @@
 ﻿import * as productivity from "./productivity";
+import * as fitIn from "./fit-in";
 
 export const prototrashPageReferences: { [key: string]: any } = {
     "productivity": productivity,
+    "fit-in": fitIn,
 };
