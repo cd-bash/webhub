0f201a2cdd5ff8b2d14bfbca01002f48f504c471
Charles Doucet <charles.douc@gmail.com>
2025-03-09 14:04:55 -0400

    feat: harvesting potatoes content

--- src/content/projects/prototrash/index.ts
+++ src/content/projects/prototrash/index.ts
@@ -1,7 +1,9 @@
 ﻿import * as productivity from "./productivity";
 import * as fitIn from "./fit-in";
+import * as harvestingPotatoes from "./harvesting-potatoes";
 
 export const prototrashPageReferences: { [key: string]: any } = {
     "productivity": productivity,
     "fit-in": fitIn,
+    "harvesting-potatoes": harvestingPotatoes,
 };
