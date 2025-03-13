24a3102b405b62dd16b175562894a4ed27c755a2
Charles Doucet <charles.douc@gmail.com>
2025-03-12 22:11:16 -0400

    feat: showers content project

--- src/content/projects/prototrash/index.ts
+++ src/content/projects/prototrash/index.ts
@@ -1,8 +1,10 @@
-﻿import * as productivity from "./productivity";
+﻿import * as showers from "./showers";
+import * as productivity from "./productivity";
 import * as fitIn from "./fit-in";
 import * as harvestingPotatoes from "./harvesting-potatoes";
 
 export const prototrashPageReferences: { [key: string]: any } = {
+    "showers": showers,
     "productivity": productivity,
     "fit-in": fitIn,
     "harvesting-potatoes": harvestingPotatoes,
