b0febfd89aed328711481475de5cb5e0a2a1941b
Charles Doucet <charles.douc@gmail.com>
2025-04-14 18:45:29 -0400

    wip: grid section content builder

--- src/views/about.ts
+++ src/views/about.ts
@@ -1,13 +1,53 @@
-﻿import {writeTitle} from "./utils";
+﻿import {writeParagraph, writeTitle} from "./utils";
 
 export type AboutContent = {
     readonly title: string;
     readonly subtitle: string;
     readonly paragraphs: ReadonlyArray<string>;
+    readonly sections: ReadonlyArray<GridSectionContent>;
 }
 
-export type AboutGrid = {
+export type GridSectionContent = {
     readonly title: string;
+    readonly categories: ReadonlyArray<GridCategoryContent>;
+}
+
+export type GridCategoryContent = {
+    readonly title: string;
+    readonly items: ReadonlyArray<GridItemContent>;
+}
+export type GridItemContent = {
+    readonly title: string;
+    readonly subtitle: string;
+    readonly text: string;
+}
+
+
+const testContent: AboutContent = {
+    title: "About CD",
+    subtitle: "A short introduction",
+    paragraphs: [
+        "This is a test paragraph. It is used to test the text utilities.",
+        "This is another test paragraph. It is used to test the text utilities.",
+        "This is a third test paragraph. It is used to test the text utilities."
+    ],
+    sections: [
+        {
+            title: "Test Section",
+            categories: [
+                {
+                    title: "Test Category",
+                    items: [
+                        {
+                            title: "Test Item",
+                            subtitle: "Test Subtitle",
+                            text: "This is a test item. It is used to test the text utilities."
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 }
 
 // --------------------------------------------------------------------------------
@@ -19,23 +59,60 @@ export function aboutView() {
 
     article.appendChild(pageTitle);
 
+    testContent.sections.forEach(section => {
+        const gridSectionElement = gridSection(section);
+        article.appendChild(gridSectionElement);
+    })
+
     return article;
 }
 
 // --------------------------------------------------------------------------------
 
-function gridSection() {
-    const gridSection = document.createElement('section');
-    const gridTitle = writeTitle("h2", "At a glance");
-    const gridContent = document.createElement('div');
-    gridContent.classList.add('grid-content');
+function gridSection(section: GridSectionContent) {
+    const gridSection = document.createElement('div');
+    const sectionTitle = writeTitle("h2", section.title);
 
-    gridSection.appendChild(gridTitle);
-    gridSection.appendChild(gridContent);
+    gridSection.appendChild(sectionTitle);
+
+    section.categories.forEach(category => {
+        const gridCategoryElement = gridCategory(category);
+        gridSection.appendChild(gridCategoryElement);
+    })
 
     return gridSection;
 }
 
-function gridItem(title: string, content: string) {
+function gridCategory(category: GridCategoryContent) {
+    const gridCategory = document.createElement('div');
+    const categoryTitle = writeTitle("h3", category.title);
 
+    gridCategory.classList.add('grid-category');
+
+    gridCategory.appendChild(categoryTitle);
+
+    category.items.forEach(item => {
+        const gridItemElement = gridItem(item);
+        gridCategory.appendChild(gridItemElement);
+    });
+
+    return gridCategory;
 }
+
+
+function gridItem(item: GridItemContent) {
+    const gridItem = document.createElement('div');
+    const itemTitle = writeTitle("h4", item.title);
+    const itemSubtitle = writeTitle("h6", item.subtitle);
+    const itemText = writeParagraph(item.text);
+
+    gridItem.classList.add('grid-item');
+
+    gridItem.appendChild(itemTitle);
+    gridItem.appendChild(itemSubtitle);
+    gridItem.appendChild(itemText);
+
+    return gridItem;
+}
+
+
