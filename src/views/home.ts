7b8369f9a4f0f55c6dcb2428344e8373fadef68c
Charles Doucet <charles.douc@gmail.com>
2025-06-01 16:28:38 -0400

    wip: home page content

--- src/views/home.ts
+++ src/views/home.ts
@@ -1,13 +1,20 @@
-﻿import {createThreeBackground, writeTitle} from "./utils";
+﻿import {createThreeBackground, writeParagraph, writeTitle} from "./utils";
 import {BackgroundChoice} from "../components/three-background";
 
 
 export function homeView() {
   const article = document.createElement('article');
-  const pageTitle = writeTitle("h1", "welcome to my hub");
+  const introBlock = document.createElement('div');
+  const pageTitle = writeTitle("h1", "pick a track");
+  const pageText = writeParagraph("Welcome to my hub! I’m CD-BASH, a creative developer based in Montreal.");
+
+  article.id = 'home-page';
+  introBlock.className = 'intro-block';
 
   createThreeBackground(BackgroundChoice.Home);
-  article.appendChild(pageTitle);
+  introBlock.appendChild(pageTitle);
+  introBlock.appendChild(pageText);
+  article.appendChild(introBlock);
 
 
 
