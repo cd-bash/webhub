64eac3d0151adbfa86f17c05b9c72e34215c8fbb
cd-bash <charles.douc@gmail.com>
2025-10-01 15:44:36 -0400

    feat: [#115] contact page

--- src/views/contact/index.ts
+++ src/views/contact/index.ts
@@ -1,12 +1,69 @@
-﻿import {writeLink, writeParagraph, writeTitle} from "../utils";
+﻿import {createWrapper, writeParagraph, writeTitle} from "../utils";
+import { createPixelGridBackground } from "../utils";
+import { createContactForm, ContactFormOptions } from "./form";
+import { contactContent } from "../../content/contact";
+import { insertAllSocials } from "../../components/socials";
+
+export type ContactPageContentStructure = {
+    readonly bio: BioContent;
+    readonly form: ContactFormOptions; 
+}
+
+type BioContent = {
+    readonly profilePictureUrl: string;
+    readonly title: string;
+    readonly bio: string[];
+}
+
+
+// --------------------------------------------------------------------------------
 
 export function contactView() {
-    const article = document.createElement('article');
-    const pageTitle = writeTitle("h1", "contact");
-    const pageSubtitle = writeTitle("h5", "let's connect");
+    const page = document.createElement('div');
 
-    article.appendChild(pageTitle);
-    article.appendChild(pageSubtitle);
+    const pixelGrid = createPixelGridBackground('top', {
+        columns: 10,
+        colors: ['#0f0f0f', '#2a2a2a', '#181818']
+    });
 
-    return article;
+    page.id = 'contact-page';
+
+    const wrapper = createWrapper();
+
+    wrapper.appendChild(bioHeader(contactContent.bio));
+    wrapper.appendChild(createContactForm(contactContent.form));
+
+    page.appendChild(pixelGrid);
+    page.appendChild(wrapper);
+    return page;
+}
+
+// --------------------------------------------------------------------------------
+
+function bioHeader(content: BioContent) {
+    const bioContainer = document.createElement('div');
+    bioContainer.className = 'bio-container';
+    
+    const profileConatiner = document.createElement('div');
+    profileConatiner.className = 'profile-picture';
+    const profileImg = document.createElement('img');
+    profileImg.src = content.profilePictureUrl;
+    profileImg.alt = "Profile Picture";
+    profileConatiner.appendChild(profileImg);
+
+    const bioTextContainer = document.createElement('div');
+    bioTextContainer.className = 'bio-text';
+    const bioTitle = writeTitle("h2", content.title);
+    bioTextContainer.appendChild(bioTitle);
+
+    content.bio.forEach(paragraph => {
+        bioTextContainer.appendChild(writeParagraph(paragraph));
+    });
+
+    bioTextContainer.appendChild(insertAllSocials());
+
+    bioContainer.appendChild(profileConatiner);
+    bioContainer.appendChild(bioTextContainer);
+
+    return bioContainer;
 }
