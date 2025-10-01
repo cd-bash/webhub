07e8b17527221e14b50d5a0216f328adae29411a
cd-bash <charles.douc@gmail.com>
2025-09-30 20:15:58 -0400

    feat: footer component

---
+++ src/components/socials/index.ts
@@ -0,0 +1,53 @@
+import GITHUB_ICON from './assets/github-icon.svg';
+import LINKEDIN_ICON from './assets/linkedin-icon.svg';
+import INSTAGRAM_ICON from './assets/instagram-icon.svg';
+
+type social = {
+    name: string;
+    url: string;
+    iconSrc: string;
+}
+
+export const socials: social[] = [
+    {
+        name: 'GitHub',
+        url: 'https://github.com/cd-bash',
+        iconSrc: GITHUB_ICON,
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/charlesdouc/',
+        iconSrc: LINKEDIN_ICON,
+    },
+    {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/cd.oucet/',
+        iconSrc: INSTAGRAM_ICON,
+    },
+]
+
+//-----------------------------------------------------------------------
+
+export function insertAllSocials() {
+    const container = document.createElement('ul');
+    container.className = 'social-icons';
+
+    socials.forEach(social => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = social.url;
+        a.target = '_blank';
+        a.rel = 'noopener noreferrer';
+
+        const img = document.createElement('img');
+        img.src = social.iconSrc;
+        img.alt = `${social.name} Icon`;
+        img.className = 'social-icon';
+
+        a.appendChild(img);
+        li.appendChild(a);
+        container.appendChild(li);
+    });
+    
+    return container;
+}
